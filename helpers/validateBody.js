f6d0635827a48557c58126b3c032171a5f682535
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-02-21 15:00:42 +0000

    hw

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -1,11 +1,25 @@
 import HttpError from "./HttpError.js";
 
-const validateBody = (schema) => {
-  const func = (req, _, next) => {
+const validateBody = async (schema) => {
+  const func = async (req, _, next) => {
+    console.log("validate body");
+
+    const { id } = req.params;
+    const contactToChange = await contactsService.getContactById(id);
+    if (!contactToChange) {
+      next(HttpError(404, "Not Found"));
+    }
+
+    if (Object.keys(req.body).length === 0) {
+      console.log("in IF");
+      next(HttpError(400, "Body must have at least one field"));
+    }
+
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
     }
+
     next();
   };
 
