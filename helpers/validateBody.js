5e40051f71e7efed574f6d9f192db22432c7dec3
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2024-02-22 09:53:35 +0000

    com-delete

--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -2,11 +2,6 @@ import HttpError from "./HttpError.js";
 
 const validateBody = (schema) => {
   const func = (req, _, next) => {
-    // console.log(req.params.id);
-    // if (!req.params.id) {
-    //   next(HttpError(404, "Not Found"));
-    // }
-
     if (Object.keys(req.body).length === 0) {
       console.log("in IF");
       next(HttpError(400, "Body must have at least one field"));
